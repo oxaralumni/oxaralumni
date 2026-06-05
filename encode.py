a05404e59c77b7ef854fc80b4b9c3eeb80301069
Harshit Gulati <harshit14012018@gmail.com>
2026-06-05 22:04:51 +0530

    Untrack docx files and helper scripts from git repository

--- encode.py
+++
@@ -1,6 +0,0 @@
-import base64
-with open('output.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-b64 = base64.b64encode(text.encode('utf-8')).decode('utf-8')
-with open('b64.txt', 'w', encoding='utf-8') as f:
-    f.write(b64)
